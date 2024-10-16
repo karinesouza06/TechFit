3b12edc07b7d54cea19b230848fbb8332f86241e
Mickaelle <151803781+karinesouza06@users.noreply.github.com>
2024-10-16 15:35:41 -0300

    Update app.py

--- TechFit/app.py
+++ TechFit/app.py
@@ -1,21 +1,84 @@
-from flask import Flask, url_for, render_template, redirect
+
+from flask import Flask, url_for, render_template, redirect, request
+import sqlite3
 
 app = Flask(__name__)
 
+def obter_conexao_banco():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('inicial.html')
 
-@app.route('/login')
+
+@app.route('/login', methods=['POST', 'GET'])
 def login():
-    return render_template('login.html')
 
-@app.route('/cadastro')
+    if request.method == 'GET':
+        return render_template ('cadastro.html')
+    
+    else:
+        nome = request.form['nome']
+        senha = request.form['senha']
+
+
+        conn = obter_conexao_banco()
+        usuario = conn.execute('SELECT * FROM dados_usuarios WHERE nome = (?) AND senha = (?)' , (nome, senha,)).fetchone()
+        conn.commit()
+
+        if usuario:
+            if senha == usuario['senha']:
+                return redirect(url_for('calcular_agua'))  #COLOQUEI PARA REDIRECIONAR PARA ESSA "PÁGINA" ENQUANTO AINDA NÃO TEMOS A DO ALUNO/PERSONAL
+        
+        else:
+            return redirect(url_for('cadastro'))
+
+
+
+@app.route('/cadastro', methods=['POST', 'GET'])
 def cadastro():
-    return render_template('cadastro.html') 
 
+    if request.method == 'GET':
+        return render_template ('cadastro.html')
+    
+    else:
+        nome = request.form['nome']
+        senha = request.form['senha']
+        confirmasenha = request.form['confirmasenha']
+        email = request.form['email']
+        telefone = request.form['telefone']
+        data_nascimento = request.form['data']
+        tipo_usuario = request.form['user-type']
+        genero = request.form['genero']
+        dias_treino = request.form['dias_treino']
+        horario_treino = request.form['horario_treino']
+        dias_trabalho = request.form['dias_trabalho']
+        horario_trabalho = request.form['horario_trabalho']
+
+
+        conn = obter_conexao_banco()
+        usuario = conn.execute('SELECT * FROM dados_usuarios WHERE nome = (?) AND email = (?)', (nome, email,)).fetchone()
+        conn.commit()
+ 
+        if usuario:
+            return redirect(url_for('inicial')) #REDIRECIONA PARA A INICIAL PARA O USUÁRIO FAZER LOGIN.
+        
+        else:
+            if senha == confirmasenha:
+                conn.execute('INSERT INTO dados_usuarios (nome, senha, email, telefone, data_nascimento, genero, tipo_usuario, dias_treino, horario_treino, dias_trabalho, horario_trabalho) VALUES (?,?,?,?,?,?,?,?,?,?,?)', (nome, senha, email, telefone, data_nascimento, genero, tipo_usuario, dias_treino, horario_treino, dias_trabalho, horario_trabalho,)).fetchone()
+                conn.commit()
+                conn.close() 
+        
+                return render_template('inicial.html')
+            
+        return render_template('cadastro.html')
+        
 @app.route('/dado_aluno')
-def dado_alunos():
+def dado_aluno():
     return render_template('form_aluno.html')
 
 @app.route('/inserir_medidas')
@@ -29,3 +92,4 @@ def calcular_imc():
 @app.route('/calcular_agua')
 def calcular_agua():
     return render_template('calculo_agua.html')
+
