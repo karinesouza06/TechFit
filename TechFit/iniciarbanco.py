d895eb8330beaeb85960462cc762289448cb2ab0
Mickaelle <151803781+karinesouza06@users.noreply.github.com>
2025-02-17 20:52:14 -0300

    Delete TechFit directory

--- TechFit/iniciarbanco.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('database.db')
-
-with open('db/flask-sqlite.sql') as arquivo:
-    conn.executescript(arquivo.read())
-
-
-#lembrar de instalar a extensão SQlite Viewer
