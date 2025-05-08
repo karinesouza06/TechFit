5a31484e9173fa7279455eae051bc22a1f205414
K A R I N E <151803781+karinesouza06@users.noreply.github.com>
2025-05-08 10:30:37 -0300

    Add files via upload

---
+++ TechFit/database/init_db.py
@@ -0,0 +1,7 @@
+import sqlite3
+
+conn = sqlite3.connect('database.db')
+
+with open('database/flask-sqlite.sql', encoding='utf-8') as arquivo:
+#with open('flask-sqlite.sql', encoding='utf-8') as arquivo:
+    conn.executescript(arquivo.read())
